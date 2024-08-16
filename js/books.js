f2e4580e18f9e9f42ae399e4a0506e56d244df9b
Junior Mabunda <mjmabunda0@gmail.com>
2024-08-16 07:45:47 +0200

    Wire the 'add book' logic to the UI

--- js/books.js
+++ js/books.js
@@ -1,7 +1,8 @@
 // Stores book objects
 let library = [];
 
-for (let i = 0; i < 5; i++) {
+// Dummy books for testing
+for (let i = 0; i < 2; i++) {
     let book = new Book(`Title ${i}`, `Author ${i}`, `${i}00 pages`, "read");
     library.push(book);
 }
@@ -13,15 +14,26 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+let dialog = document.querySelector("dialog");
+dialog.addEventListener("close", () => {
+    AddBookToLibrary();
+    displayBooks();
+});
+
 function AddBookToLibrary() {
     // add a book to the library
+    let new_book = new Book(
+        document.querySelector("#title").value,
+        document.querySelector("#author").value,
+        document.querySelector("#pages").value,
+        document.querySelector("#read").value,
+    );
+    library.push(new_book);
 }
 
 document.querySelector("#add-btn").addEventListener("click", () => {
-    AddBookToLibrary();
-
-    // Update the UI
-    displayBooks();
+    dialog.showModal();
+    
 });
 
 // Updates the display by rendering all `book`
@@ -34,13 +46,13 @@ function displayBooks() {
     document.querySelector("main").appendChild(books_container);
 
     // Create DOM elements for each book and display it.
-    for (let book of library) {
+    for (let i = 0; i < library.length; i++) {
         let div = document.createElement("div");
         div.classList.add("book");
 
         // Create elements for each book property
         // and add to book element to be displayed.
-        for (const value of Object.values(book)) {
+        for (const value of Object.values(library[i])) {
             let elem = document.createElement("p");
             elem.textContent = value;
 
